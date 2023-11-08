8927456f0ac2d7c7fe2f6e86515307cf89606477
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2023-11-08 15:51:50 +0700

    finish tutorial 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shoppinglistmobile/menu.dart';
+import 'package:shoppinglistmobile/screens/menu.dart';
 
+// /Users/joshuajodrian/repos/flutter/bin
 void main() {
   runApp(const MyApp());
 }
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: MyHomePage()  ,
+      home: MyHomePage(),
     );
   }
 }
